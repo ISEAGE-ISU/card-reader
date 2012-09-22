ecf38be10bc498c244c422d3274af275a6b879ba
Michael Davis <mike.philip.davis@gmail.com>
2012-09-22 00:57:18 -0500

    Changes to make the card reader

--- main.py
+++ main.py
@@ -24,12 +24,67 @@ from google.appengine.ext.webapp import blobstore_handlers
 
 import models
 import utils
+import logging
+
+import re
+import json
+import urllib2
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
-
         path = os.path.join(os.path.dirname(__file__), 'templates/home.html')
         self.response.out.write(template.render(path, {}))
+
+    def post(self):
+        try:
+            uid = self.request.POST.get('uid')
+
+            if uid.startswith(';600957'):
+                uid = uid[7:16]
+
+            if re.match("^[0-9]*$", uid):
+
+                url = 'http://www.fpm.iastate.edu/roomscheduling/student_form/ajax_getISUdata.asp?returnfield=json_string&uid=%s' % uid
+                response = urllib2.urlopen(url)
+                output = response.read()
+
+            else:
+                url = 'http://www.fpm.iastate.edu/roomscheduling/student_form/ajax_getISUdata.asp?returnfield=json_string&email=%s@iastate.edu' % uid
+                response = urllib2.urlopen(url)
+                output = response.read()
+
+            json_output = json.loads(output[12:])
+            name = json_output['name']
+            email = json_output['email']
+
+            submission = models.Submission.all().filter('uid =', uid).fetch(1)
+            if submission:
+                context = {'name' : name}
+                context['refresh'] = True
+
+                path = os.path.join(os.path.dirname(__file__), 'templates/already_submitted.html')
+                self.response.out.write(template.render(path, context))
+
+            else:
+
+                if 'NOT FOUND' in name:
+                    context={'refresh' : True}
+                    path = os.path.join(os.path.dirname(__file__), 'templates/not_found.html')
+                    self.response.out.write(template.render(path, context))
+
+                else:
+                    submission = models.Submission(name=name, email=email, uid=uid)
+                    submission.save()
+
+                    context = {'name' : name}
+                    context['refresh'] = True
+
+                    path = os.path.join(os.path.dirname(__file__), 'templates/thank_you.html')
+                    self.response.out.write(template.render(path, context))
+        except Exception, e:
+            path = os.path.join(os.path.dirname(__file__), 'templates/error.html')
+            self.response.out.write(template.render(path, context))
+
 
 app = webapp2.WSGIApplication([('/', MainHandler),
 
--- models.py
+++ models.py
@@ -3,3 +3,8 @@ __author__ = 'mike.davis'
 from google.appengine.ext import db
 from google.appengine.ext import blobstore
 
+class Submission(db.Model):
+    name = db.StringProperty()
+    time = db.DateTimeProperty(auto_now_add=True)
+    email = db.StringProperty()
+    uid = db.StringProperty()
